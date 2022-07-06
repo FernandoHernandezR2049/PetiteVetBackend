08714ee6e0c4f1b58a5013458d1732c2de501ea0
ItzelGranados <itzelgranados13@gmail.com>
2022-07-06 11:39:05 -0500

    Add: Starter files for Springboot project

---
+++ src/main/java/com/petitevet/backPetite/BackPetiteApplication.java
@@ -0,0 +1,13 @@
+package com.petitevet.backPetite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPetiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackPetiteApplication.class, args);
+	}
+
+}
